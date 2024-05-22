e67bc47e270153028bfe6c3c710a81741fd94499
Kamva <you@example.com>
2024-05-22 23:28:25 +0200

    weather app commit 2

--- Tutorial 2/weather-app/src/weather/WeatherInfo.js
+++ Tutorial 2/weather-app/src/weather/WeatherInfo.js
@@ -2,8 +2,11 @@ function WeatherInfo({ weatherData, cityName }) {
 
     return (
             <div className="weather-info text-center card bg-light">
-                </div>
-
-              )
+            <h3>{cityName}</h3>
+                <i className="bi bi-cloud-haze text-center display-2 text-warning"></i>
+                {
+                    typeof weatherData.main == "undefined" ? (
+                        <p>No data found</p>
+                    ) : (
 
             }
