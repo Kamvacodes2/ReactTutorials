fd135572527f08fb5b0cd7cf22ef619e1615ca4f
Kamva <you@example.com>
2024-05-23 09:15:40 +0200

    gallery-app

---
+++ Tutorial 4/gallery-app/src/App.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+import React from 'react';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+import Gallery from "./portfolio-gallery/Gallery";
+
+function App () {
+    return (
+      <Gallery/>
+    )
+}
+export default App;
