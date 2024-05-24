b278464c552d344fe06ae4c2d78248b5172e14f1
Kamva <mtikasoga96@gmail.com>
2024-05-24 15:19:28 +0200

    shopping cart

--- Tutorial 5/react-18-shopping-cart/src/App.js
+++ Tutorial 5/react-18-shopping-cart/src/App.js
@@ -2,21 +2,23 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 import Basket from "./components/Basket";
 import data from './data';
-import { useState } from "react";
+import { useDeferredValue, useEffect, useState, useTransition } from "react";
 
 
 function App() {
-  const { products } = data;
   const [cartItems, setCartItems] = useState([])
+  const { products } = data;
 
   const onAdd = (product) => {
     const exist = cartItems.find((x) => x.id === product.id);
     if (exist) {
-      const newCartItems = cartItems.map((x) => x.id === product.id ? { ...exist, qty: exist + 1 } : x);
+      const newCartItems = cartItems.map((x) => x.id === product.id ? { ...exist, qty: exist.qty + 1 } : x);
       setCartItems(newCartItems);
+      localStorage.setItem('cartItems', JSON.stringify(newCartItems));
     } else {
       const newCartItems = [...cartItems, { ...product, qty: 1 }];
       setCartItems(newCartItems)
+      localStorage.setItem('cartItems', JSON.stringify(newCartItems));
     }
   }
 
@@ -25,17 +27,30 @@ function App() {
     if (exist.qty === 1) {
       const newCartItems = cartItems.filter((x) => x.id !== product.id);
       setCartItems(newCartItems);
+      localStorage.setItem('cartItems', JSON.stringify(newCartItems));
     } else {
       const newCartItems = cartItems.map((x)=> x.id === product.id? {...exist, qty: exist.qty - 1 }: x);
+      setCartItems(newCartItems);
+      localStorage.setItem('cartItems', JSON.stringify(newCartItems));
     }
-  }
+  };
 
-  return (
+  const [isPending, startTransition] = useTransition();
+
+  useEffect(() => {
+    startTransition(()=>{setCartItems(localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')): []);})
+    
+  }, []);
+
+  const cartItemsCount = useDeferredValue(cartItems.length)
+
+  return isPending?  ( <div>Loading...</div>)
+  : (
     <div>
-      <Header countCartItems={cartItems.length} />
+      <Header countCartItems={cartItemsCount} />
       <div className="row">
         <Main cartItems={cartItems} onAdd={onAdd} onRemove={onRemove} products={products} />
-        <Basket />
+        <Basket cartItems={cartItems} onAdd={onAdd} onRemove={onRemove}/>
       </div>
     </div>
   );
