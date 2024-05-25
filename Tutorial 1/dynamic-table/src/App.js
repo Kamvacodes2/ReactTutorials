ac2ed15c5896e23b15e2466867c36efc7495641f
Kamva <mtikasoga96@gmail.com>
2024-05-25 14:17:45 +0200

    Dynamic Table

---
+++ Tutorial 1/dynamic-table/src/App.js
@@ -0,0 +1,13 @@
+// import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import DynamicTable from './dynamic-table/DynamicTable';
+
+function App() {
+  return (
+    <div>
+      <DynamicTable />
+    </div>
+  );
+}
+
+export default App;
