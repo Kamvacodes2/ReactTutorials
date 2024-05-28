cd87f2cebbf37021c886d6f5047a903f1c575368
Kamva <mtikasoga96@gmail.com>
2024-05-28 13:44:49 +0200

    Firebase Application_Realtor Update

--- Tutorial 7/firebase-app/src/App.js
+++ Tutorial 7/firebase-app/src/App.js
@@ -1,10 +1,28 @@
-// import logo from './logo.svg';
+
+import React from "react";
 import "tailwindcss/tailwind.css"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import Profile from "./pages/Profile";
+import SignUp from "./pages/SignUp";
+import Offers from "./pages/Offers";
+import ForgotPassword from "./pages/ForgotPassword";
+
 
 function App() {
   return (
     <div>
-        <h1 className='text-2xl'>Hello World</h1>
+        <Router>
+          <Routes>
+            <Route path="/" Component={Home}/>
+            <Route path="/profile" Component={Profile}/>
+            <Route path="/sign-in" Component={SignIn}/>
+            <Route path="/sign-up" Component={SignUp}/>
+            <Route path="/forgot-password" Component={ForgotPassword}/>
+            <Route path="/offers" Component={Offers}/>
+          </Routes>
+        </Router>
     </div>
   );
 }
