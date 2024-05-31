c5120d233865c7c30f2709ba997f01c2e33e6968
Kamva <mtikasoga96@gmail.com>
2024-05-31 17:38:13 +0200

    create routing and functionality of signing in and out of app

--- Tutorial 7/firebase-app/src/App.js
+++ Tutorial 7/firebase-app/src/App.js
@@ -4,6 +4,7 @@ import "tailwindcss/tailwind.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
+import PrivateRoute from "./components/PrivateRoute";
 import Profile from "./pages/Profile";
 import SignUp from "./pages/SignUp";
 import Offers from "./pages/Offers";
@@ -19,7 +20,9 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" Component={Home} />
+          <Route path="/profile" element={<PrivateRoute/>}>
           <Route path="/profile" Component={Profile} />
+          </Route>
           <Route path="/sign-in" Component={SignIn} />
           <Route path="/sign-up" Component={SignUp} />
           <Route path="/forgot-password" Component={ForgotPassword} />
