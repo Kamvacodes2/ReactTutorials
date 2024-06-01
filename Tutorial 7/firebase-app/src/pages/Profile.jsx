b460da58afd0064f4cd182a4bb0929caad5b9ffb
Kamva <mtikasoga96@gmail.com>
2024-06-01 13:16:33 +0200

    add profile editing to firebase app

--- Tutorial 7/firebase-app/src/pages/Profile.jsx
+++ Tutorial 7/firebase-app/src/pages/Profile.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom"
 export default function Profile() {
     const auth = getAuth()
     const navigate = useNavigate()
+    const [changeDetail, setChangeDetail] = useState(false)
     const [formData, setFormData] = useState({
         name: auth.currentUser.displayName,
         email: auth.currentUser.email
@@ -15,6 +16,13 @@ export default function Profile() {
         navigate('/')
     }
     const {name, email} = formData
+
+    function onChange(e) {
+        setFormData((prevState)=> ({
+            ...prevState, [e.target.id]: e.target.value
+        }))
+
+    }
     return (
         <>
             <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
@@ -22,12 +30,13 @@ export default function Profile() {
                 <div className="w-full md:w-[50%] mt-6 px-3">
                     <form action="">
                         {/*Name Input*/}
-                        <input type="text" id="name" value={name} disabled className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out"/>
+                        <input type="text" id="name" value={name} disabled={!changeDetail} className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out"
+                        onChange={onChange}/>
                         {/*Email Input*/}
                         <input type="email" id="email" value={email} disabled className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out"/>
                         <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
                             <p className="flex items-center">Do you want to change your name?
-                                <span className="text-red-600 hover:text-red-800 transition ease-in-out duration-200 ml-1 cursor-pointer"> Edit</span>
+                                <span onClick={() => setChangeDetail((prevState)=> !prevState)} className="text-red-600 hover:text-red-800 transition ease-in-out duration-200 ml-1 cursor-pointer"> {changeDetail ? "Apply change" : "Edit"}</span>
                             </p>
                             <p onClick={onLogout} className="text-blue-600 hover:text-blue-800 transition ease-in-out duration-200 ml-1 cursor-pointer">
                                 Sign out
