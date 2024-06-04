9dedf12cdb912b83e3a2b221988a5f092b889950
Kamva <mtikasoga96@gmail.com>
2024-06-04 22:06:20 +0200

    add image upload functionality to firebase app

--- Tutorial 7/firebase-app/src/pages/CreateDeal.jsx
+++ Tutorial 7/firebase-app/src/pages/CreateDeal.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
 import Spinner from '../components/Spinner';
 import { toast } from 'react-toastify';
-import { getStorage, ref, uploadString } from "firebase/storage";
+import { getStorage, ref, uploadBytesResumable } from "firebase/storage";
 import { getAuth } from 'firebase/auth';
+import { v4 as uuidv4 } from 'uuid';
 
 export default function CreateDeal() {
     const auth = getAuth
@@ -26,16 +27,16 @@ export default function CreateDeal() {
         if (e.target.value === "false") {
             boolean = false;
         }
-          // This is meant for file uploads
+        // This is meant for file uploads
         if (e.target.files) {
-            setFormData((prevState)=>({
+            setFormData((prevState) => ({
                 ...prevState,
                 images: e.target.files
             }))
         }
         //For text or booleans or numbers
-        if(!e.target.files) {
-            setFormData((prevState)=>({
+        if (!e.target.files) {
+            setFormData((prevState) => ({
                 ...prevState, [e.target.id]: boolean ?? e.target.value
             }))
         }
@@ -51,21 +52,56 @@ export default function CreateDeal() {
         }
 
         async function storeImage(image) {
-            return new Promise((res, reject)=>{
+            return new Promise((res, reject) => {
                 const storage = getStorage()
-                const filename = `${auth.currentUser.uid}-${image.name}-`
-            })
-        }
+                const filename = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`
+                const storageRef = ref(storage, filename)
+                const uploadTask = uploadBytesResumable(storageRef, image);
+                // Listen for state changes, errors, and completion of the upload.
+                uploadTask.on('state_changed',
+                    (snapshot) => {
+                        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                        console.log('Upload is ' + progress + '% done');
+                        switch (snapshot.state) {
+                            case 'paused':
+                                console.log('Upload is paused');
+                                break;
+                            case 'running':
+                                console.log('Upload is running');
+                                break;
+                        }
+                    },
+                    (error) => {
+                        // A full list of error codes is available at
+                        // https://firebase.google.com/docs/storage/web/handle-errors
+                        switch (error.code) {
+                            case 'storage/unauthorized':
+                                // User doesn't have permission to access the object
+                                break;
+                            case 'storage/canceled':
+                                // User canceled the upload
+                                break;
 
-        const imgUrls = await Promise.all([...images].map((image)=>storeImage(image)))
-        .catch((error)=>{
-            setLoading(false)
-            toast.error('Images not uploaded');
-            return;
-        })
+                            // ...
+
+                            case 'storage/unknown':
+                                // Unknown error occurred, inspect error.serverResponse
+                                break;
+                        }
+                    })
+                }
+        )}
+
+        const imgUrls = await Promise.all([...images].map((image) => storeImage(image)))
+            .catch((error) => {
+                setLoading(false)
+                toast.error('Images not uploaded');
+                return;
+            })
     }
-    if(loading) {
-        return <Spinner/>
+    if (loading) {
+        return <Spinner />
     }
     const { brand, model, year, type, kms, description, price, images } = formData
     return (
@@ -112,21 +148,21 @@ export default function CreateDeal() {
                 <textarea type="text" id='description' value={description} onChange={onChange} placeholder='Description' required
                     className='w-full px-4 py-2 text-lg text-gray-700 bg-white border border-gray-300 rounded transition duration-150
             ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 mb-6'/>
-            <div className='mb-6'>
-                <div className='text-lg font-semibold'>
-                    <p>Price</p>
-                    <input type="number" id='price' value={price} onChange={onChange} min={5000} max={20000000} required className='w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded 
+                <div className='mb-6'>
+                    <div className='text-lg font-semibold'>
+                        <p>Price</p>
+                        <input type="number" id='price' value={price} onChange={onChange} min={5000} max={20000000} required className='w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded 
                     transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 text-center' />
+                    </div>
                 </div>
-            </div>
-            <div className='mb-6'>
-                <p className='text-lg font-semibold'>Images</p>
-                <p className='text-gray-600'>The first image will be the cover (max 6)</p>
-                <input type="file" id="images" onChange={onChange} accept='.jpg, .png,.jpeg' multiple required 
-                className='w-full px-3 py-1.5 text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out
+                <div className='mb-6'>
+                    <p className='text-lg font-semibold'>Images</p>
+                    <p className='text-gray-600'>The first image will be the cover (max 6)</p>
+                    <input type="file" id="images" onChange={onChange} accept='.jpg, .png,.jpeg' multiple required
+                        className='w-full px-3 py-1.5 text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out
                 focus:bg-white focus:border-slate-600'/>
-            </div>
-            <button type='submit' className='mb-6 w-full px-7 py-3 bg-blue-600 text-white font-medium text-sm uppercase
+                </div>
+                <button type='submit' className='mb-6 w-full px-7 py-3 bg-blue-600 text-white font-medium text-sm uppercase
             rounded shadow-md  hover:bg-blue-800 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg active:bg-blue-800
             transition duration-150 ease-in-out'>Create Offer</button>
             </form>
