f35fac296eab13aea9281f662fb7c5fb18411b19
Kamva <mtikasoga96@gmail.com>
2024-06-12 13:48:58 +0200

    Added dynamic deal information display to listing page

--- Tutorial 7/firebase-app/src/pages/Deal.jsx
+++ Tutorial 7/firebase-app/src/pages/Deal.jsx
@@ -13,7 +13,7 @@ export default function Deal() {
     const params = useParams();
     const [deal, setDeals] = useState(null)
     const [loading, setLoading] = useState(true)
-    const [shareLinkCopied,setShareLinkCopied] = useState(false)
+    const [shareLinkCopied, setShareLinkCopied] = useState(false)
     SwiperCore.use([Autoplay, Navigation, Pagination])
     useEffect(() => {
         async function fetchUserDeal() {
@@ -46,18 +46,37 @@ export default function Deal() {
                 ))}
             </Swiper>
             <div className='fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12
-            flex justify-center items-center' onClick={()=> {
-                navigator.clipboard.writeText(window.location.href) 
-                setShareLinkCopied(true)
-                setTimeout(()=>{
-                    setShareLinkCopied(false)
-                }, 2000)
-            }}>
-                <FaShare className='text-lg font-light text-slate-500'/>
+            flex justify-center items-center' onClick={() => {
+                    navigator.clipboard.writeText(window.location.href)
+                    setShareLinkCopied(true)
+                    setTimeout(() => {
+                        setShareLinkCopied(false)
+                    }, 2000)
+                }}>
+                <FaShare className='text-lg font-light text-slate-500' />
             </div>
             {shareLinkCopied && (
                 <p className='fixed top-[23%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-2'>Link Copied</p>
             )}
+            <div className='m4 max-w-6xl lg:mx-auto p-4 rounded-lg border-3 shadow-lg bg-white lg:space-x-5'>
+                <div className=' w-full h-[200px] lg-[400px] mt'>
+                    <p className='text-2xl font-bold mb-3 text-blue-900'>
+                        {deal.brand} - {deal.model} - R {deal.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                    </p>
+                    <div className='flex justify-start items-center space-x-4 w-[75%]'>
+                        <p className="bg-red-800 w-full max-w-[200px] rounded-md p-1 text-white text-center font-semibold shadow-md">
+                           Total kms: {deal.kms}
+                        </p>
+                        <div>
+                            <p className='bg-green-800 w-full max-w-[200px] rounded-md p-1 text-white text-center font-semibold
+                        shadow-md'>{deal.type === "manual" ? "Manual" : "Automatic"}</p>
+                        </div>
+                    </div>
+                    <p className='mt-3 mb-3'>
+                        <span className='font-semibold'>Description - </span>
+                        {deal.description}</p>
+                </div>
+            </div>
         </main>
     )
 }
