5d474b9cab9af760b3283e6fa4bdd1dde8367c33
Kamva <mtikasoga96@gmail.com>
2024-06-13 10:50:03 +0200

    adding functionality to allow user to contact seller

--- Tutorial 7/firebase-app/src/pages/Deal.jsx
+++ Tutorial 7/firebase-app/src/pages/Deal.jsx
@@ -8,12 +8,16 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore, { EffectFade, Autoplay, Navigation, Pagination } from 'swiper';
 import 'swiper/css/bundle';
 import { FaShare } from "react-icons/fa";
+import { getAuth } from 'firebase/auth';
+import Contact from '../components/Contact';
 
 export default function Deal() {
+    const auth = getAuth();
     const params = useParams();
     const [deal, setDeals] = useState(null)
     const [loading, setLoading] = useState(true)
     const [shareLinkCopied, setShareLinkCopied] = useState(false)
+    const [contactOwner, setContactOwner] = useState(false)
     SwiperCore.use([Autoplay, Navigation, Pagination])
     useEffect(() => {
         async function fetchUserDeal() {
@@ -58,24 +62,34 @@ export default function Deal() {
             {shareLinkCopied && (
                 <p className='fixed top-[23%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-2'>Link Copied</p>
             )}
-            <div className='m4 max-w-6xl lg:mx-auto p-4 rounded-lg border-3 shadow-lg bg-white lg:space-x-5'>
-                <div className=' w-full h-[200px] lg-[400px] mt'>
+            <div className='m4 max-w-3xl lg:mx-auto p-4 rounded-lg border-3 shadow-lg bg-white lg:space-x-5'>
+                <div className=' w-full'>
                     <p className='text-2xl font-bold mb-3 text-blue-900'>
                         {deal.brand} - {deal.model} - R {deal.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
                     </p>
                     <div className='flex justify-start items-center space-x-4 w-[75%]'>
                         <p className="bg-red-800 w-full max-w-[200px] rounded-md p-1 text-white text-center font-semibold shadow-md">
-                           Total kms: {deal.kms}
+                            Total kms: {deal.kms.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
                         </p>
                         <div>
                             <p className='bg-green-800 w-full max-w-[200px] rounded-md p-1 text-white text-center font-semibold
                         shadow-md'>{deal.type === "manual" ? "Manual" : "Automatic"}</p>
                         </div>
                     </div>
-                    <p className='mt-3 mb-3'>
+                    <p className='mt-3 mb-4'>
                         <span className='font-semibold'>Description - </span>
                         {deal.description}</p>
                 </div>
+                {deal.userRef !== auth.currentUser?.uid && !contactOwner &&  (
+                    <div className='mt-6'>
+                        <button onClick={() =>setContactOwner(true)} className='px-7 py-3 bg-blue-600 text-white font-medium text-sm uppercase rounded 
+                                shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg  text-center transition duration-150 
+                                ease-in-out '>Contact Owner</button>
+                    </div>
+                )}
+                {contactOwner && (
+                    <Contact userRef={deal.userRef} deal={deal}/>
+                )}
             </div>
         </main>
     )
