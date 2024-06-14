6f8aa406369ef4785cac5865d5772623d6baf199
Kamva <mtikasoga96@gmail.com>
2024-06-14 13:44:46 +0200

    finalized slider component on home page

--- Tutorial 7/firebase-app/src/pages/Home.jsx
+++ Tutorial 7/firebase-app/src/pages/Home.jsx
@@ -1,24 +1,7 @@
-import { useEffect } from "react";
 import Slider from "../components/Slider";
-import { collection, getDocs, limit, orderBy, query } from "firebase/firestore";
-import { db } from "../firebase";
+
+
 
 export default function Home() {
-    useEffect(()=>{
-        async function fetchUserDeal() {
-            const dealsRef = collection(db, 'car-deals')
-            const q = query(dealsRef, orderBy('timestamp', 'desc'), limit(5))
-            const querySnap = await getDocs(q)
-            let deals = []
-            querySnap.forEach((doc)=>{
-                deals.push(doc)
-            })
-        }
-        fetchUserDeal()
-    }, [])
-    return (
-        <div>
-            <Slider/>
-        </div>
-    )
+    return  <><Slider/></>
 }
