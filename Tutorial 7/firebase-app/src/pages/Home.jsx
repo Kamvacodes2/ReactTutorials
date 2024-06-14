b614cbe6697c524d53aa0d7d5ba4e7a9c1522cd4
Kamva <mtikasoga96@gmail.com>
2024-06-14 08:47:45 +0200

    adding promise and fetching function to slider

--- Tutorial 7/firebase-app/src/pages/Home.jsx
+++ Tutorial 7/firebase-app/src/pages/Home.jsx
@@ -1,6 +1,18 @@
+import { useEffect } from "react";
+import Slider from "../components/Slider";
+import { collection, query } from "firebase/firestore";
 
 export default function Home() {
+    useEffect(()=>{
+        async function fetchUserDeal() {
+            const dealsRef = collection(db, 'car-deals')
+            const q = query(dealsRef)
+        }
+        fetchUserDeal()
+    }, [])
     return (
-        <div>Home</div>
+        <div>
+            <Slider/>
+        </div>
     )
 }
