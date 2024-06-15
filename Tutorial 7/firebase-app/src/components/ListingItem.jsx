79883b62a48dbf361fab80ad2ffde60da670f0f7
Kamva <mtikasoga96@gmail.com>
2024-06-15 19:53:30 +0200

    display products on the actual offer page

--- Tutorial 7/firebase-app/src/components/ListingItem.jsx
+++ Tutorial 7/firebase-app/src/components/ListingItem.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import Moment from 'react-moment';
 import { FaTrashAlt } from "react-icons/fa";
 import { MdEdit } from "react-icons/md";
 
-
 export default function ListingItem({ deal, id, onDelete, onEdit }) {
     return (
-        <li className=' relative bg-white flex flex-col justify-between items-center shadow-md hover:shadow-xl rounded-md overflow-hidden
-        transition-shadow duration-150'>
-            <Link to={`/category/${deal.type}/${id}`}>
+        <li className='relative bg-white flex flex-col justify-between items-center shadow-md hover:shadow-xl rounded-md overflow-hidden
+        transition-shadow duration-150 p-4 m-4'>
+            <Link to={`/category/${deal.type}/${id}`} className='w-full'>
                 <img className='h-[170px] w-full object-cover hover:scale-150 transition-scale duration-200 ease-in'
                     loading='lazy' src={deal.imgUrls[0]} alt="" />
                 <Moment className='absolute top-2 left-2 bg-[#3377cc] text-white uppercase text-xs font-semibold rounded-md
@@ -26,10 +25,10 @@ export default function ListingItem({ deal, id, onDelete, onEdit }) {
                 </div>
             </Link>
             {onDelete && (
-                <FaTrashAlt className='absolute bottom-2 right-2 h-[14] cursor-pointer text-red-500' onClick={()=>onDelete(deal.id)}/>
+                <FaTrashAlt className='absolute bottom-2 right-2 h-[14] cursor-pointer text-red-500' onClick={() => onDelete(deal.id)} />
             )}
             {onEdit && (
-                <MdEdit className='absolute bottom-2 right-7 h-4 cursor-pointer text-gray-500' onClick={()=>onEdit(deal.id)}/>
+                <MdEdit className='absolute bottom-2 right-7 h-4 cursor-pointer text-gray-500' onClick={() => onEdit(deal.id)} />
             )}
         </li>
     )
